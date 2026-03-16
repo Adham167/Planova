8db4ed14dc8e9fc1d401ea4b3ccfbee67079c3c1
2laaElsayed <alaaelssayed23@example.com>
2026-03-16 02:32:05 +0200

    ِÙAddgn Up

--- lib/features/auth/screens/sign_in_screen.dart
+++ lib/features/auth/screens/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/custom_text_field.dart';
 import '../widgets/auth_button.dart';
+import '../screens/sign_up_screen.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
@@ -44,7 +45,7 @@ class SignInScreen extends StatelessWidget {
               const SizedBox(height: 20),
               _buildOrDivider(),
               const SizedBox(height: 25),
-              _buildSignUpRow(),
+              _buildSignUpRow(context),
               const SizedBox(height: 30),
             ],
           ),
@@ -66,14 +67,29 @@ class SignInScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildSignUpRow() {
+  Widget _buildSignUpRow(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const Text("Don't Have an Account ? ", style: TextStyle(color: Colors.grey, fontSize: 13)),
         GestureDetector(
-          onTap: () {},
-          child: const Text("Sign Up", style: TextStyle(color: Color(0xFF9BA4FF), fontWeight: FontWeight.bold, fontSize: 13)),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SignUpScreen()),
+            );
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(
+              color: Color(0xFF9BA4FF),
+              fontWeight: FontWeight.bold,
+              fontSize: 13,
+              decoration: TextDecoration.underline,
+              decorationColor: Color(0xFF9BA4FF),
+              decorationThickness: 1.5,
+            ),
+          ),
         ),
       ],
     );
