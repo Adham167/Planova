7d36193d8261c35fc28edab2a3d2b89db2e09b06
2laaElsayed <alaaelssayed23@example.com>
2026-07-04 16:19:22 +0300

    resolve overflow in sign in screen

--- lib/features/auth/screens/sign_in_screen.dart
+++ lib/features/auth/screens/sign_in_screen.dart
@@ -19,7 +19,13 @@ class _SignInScreenState extends State<SignInScreen> {
   final passwordController = TextEditingController();
 
   bool isPasswordHidden = true;
-  bool isLoading = false;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   void handleLogin() async {
     if (emailController.text.trim().isEmpty ||
@@ -66,79 +72,86 @@ class _SignInScreenState extends State<SignInScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 80),
-              const Text(
-                "Sign In",
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF1D264F),
-                ),
-              ),
-              const SizedBox(height: 12),
-              const Text(
-                "Welcome back, you've been missed",
-                style: TextStyle(fontSize: 14, color: Colors.grey),
-              ),
-              const SizedBox(height: 50),
-
-              CustomAuthField(
-                hint: "Email Address",
-                icon: Icons.email_outlined,
-                controller: emailController,
-              ),
-
-              const SizedBox(height: 20),
-
-              CustomAuthField(
-                hint: "Password",
-                icon: Icons.lock_outline,
-                isPassword: isPasswordHidden,
-                controller: passwordController,
-                suffixIcon: isPasswordHidden
-                    ? Icons.visibility_off_outlined
-                    : Icons.visibility_outlined,
-                onSuffixTap: () {
-                  setState(() {
-                    isPasswordHidden = !isPasswordHidden;
-                  });
-                },
-              ),
-
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    context.push('/resetPassword');
-                  },
-                  child: const Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 13,
-                      decoration: TextDecoration.underline,
+        child: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false, 
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 60), 
+                    const Text(
+                      "Sign In",
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF1D264F),
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 12),
+                    const Text(
+                      "Welcome back, you've been missed",
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 40),
+
+                    CustomAuthField(
+                      hint: "Email Address",
+                      icon: Icons.email_outlined,
+                      controller: emailController,
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    CustomAuthField(
+                      hint: "Password",
+                      icon: Icons.lock_outline,
+                      isPassword: isPasswordHidden,
+                      controller: passwordController,
+                      suffixIcon: isPasswordHidden
+                          ? Icons.visibility_off_outlined
+                          : Icons.visibility_outlined,
+                      onSuffixTap: () {
+                        setState(() {
+                          isPasswordHidden = !isPasswordHidden;
+                        });
+                      },
+                    ),
+
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          context.push('/resetPassword');
+                        },
+                        child: const Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 13,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Spacer(), 
+                    const SizedBox(height: 20),
+
+                    context.watch<AuthProvider>().isLoading
+                        ? const CircularProgressIndicator(color: Color(0xFF9BA4FF))
+                        : AuthButton(text: "Sign In", onPressed: handleLogin),
+
+                    const SizedBox(height: 20),
+                    _buildOrDivider(),
+                    const SizedBox(height: 25),
+                    _buildSignUpRow(context),
+                    const SizedBox(height: 20),
+                  ],
                 ),
               ),
-
-              const Spacer(),
-
-              context.watch<AuthProvider>().isLoading
-                  ? const CircularProgressIndicator(color: Color(0xFF9BA4FF))
-                  : AuthButton(text: "Sign In", onPressed: handleLogin),
-
-              const SizedBox(height: 20),
-              _buildOrDivider(),
-              const SizedBox(height: 25),
-              _buildSignUpRow(context),
-              const SizedBox(height: 30),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
